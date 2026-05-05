78a9476082cb3913e31f2461b518ab82fc2edd8b
Wen_YG <361583983@qq.com>
2026-05-05 19:45:08 +0900

    remove favorite

--- models/favorite.go
+++ models/favorite.go
@@ -6,9 +6,3 @@ type FavoriteItem struct {
 	CocktailID string    `bson:"cocktailId" json:"cocktailId"`
 	CreatedAt  time.Time `bson:"createdAt" json:"createdAt"`
 }
-type Favorite struct {
-	ID         string    `bson:"_id,omitempty" json:"id"`
-	UserID     string    `bson:"userId" json:"userId"`
-	CocktailID string    `bson:"cocktailId" json:"cocktailId"`
-	CreatedAt  time.Time `bson:"createdAt" json:"createdAt"`
-}
